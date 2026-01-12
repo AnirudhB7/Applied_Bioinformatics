b6696fd47a5d0c621c95b9aa6ffb7462d5ecc776
Anirudh Bhatia <161936725+AnirudhB7@users.noreply.github.com>
2026-01-13 04:35:03 +0530

    Add files via upload

---
+++ programming_assignment_1.py
@@ -0,0 +1,11 @@
+
+
+##################################################
+# QUESTION 1                                     #
+# Add code here that will prints "Hello, world!" #
+# to the screen, without the quotes.             #
+# The output should occupy one line of text      #
+# edit below this line                           #
+##################################################
+
+print ("Hello, world!")
